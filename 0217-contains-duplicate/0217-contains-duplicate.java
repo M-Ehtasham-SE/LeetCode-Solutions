2866ee6562913888f41e010878dd6fde449ff839
EHTASHAM SAEED <ehtashamsd@gmail.com>
2026-07-19 13:03:57 +0500

    Time: 24 ms (26.19%), Space: 81.4 MB (82.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int left=0;
-        int right=nums.length-1;
-        while(left<right){
-            if(nums[left]==nums[left+1]){
+        Arrays.sort(nums); 
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
-            left++;
         }
         return false;
+        // Arrays.sort(nums);
+        // int left=0;
+        // int right=nums.length-1;
+        // while(left<right){
+        //     if(nums[left]==nums[left+1]){
+        //         return true;
+        //     }
+        //     left++;
+        // }
+        // return false;
     }
 }
